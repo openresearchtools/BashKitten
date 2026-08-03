7978868cefc16c3f8219a5bc250c79f48ec12de2
Michael Kaply <mozilla@kaply.com>
2026-08-03 19:34:26 +0000

    Bug 2051921 - Add policy for controlling default browser in prefs.  a=diannaS DONTBUILD
    Original Revision: https://phabricator.services.mozilla.com/D311372
    
    Differential Revision: https://phabricator.services.mozilla.com/D316101

--- browser/components/enterprisepolicies/Policies.sys.mjs
+++ browser/components/enterprisepolicies/Policies.sys.mjs
@@ -894,6 +894,23 @@ export var Policies = {
     },
   },
 
+  DefaultBrowserSettingEnabled: {
+    onBeforeAddons(manager, param) {
+      if (param) {
+        lazy.PoliciesUtils.setAndLockPref(
+          "browser.shell.checkDefaultBrowser",
+          true
+        );
+      } else {
+        manager.disallowFeature("setDefaultBrowser");
+        lazy.PoliciesUtils.setAndLockPref(
+          "browser.shell.checkDefaultBrowser",
+          false
+        );
+      }
+    },
+  },
+
   DefaultDownloadDirectory: {
     onBeforeAddons(manager, param) {
       PoliciesUtils.setDefaultPref(
