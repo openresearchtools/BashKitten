add219978479e4c19ca59c6f258a5cf7b1281640
ffxbld <ffxbld@mozilla.com>
2026-08-10 11:37:12 +0000

    No Bug, mozilla-esr153 repo-update remote-settings ct-logs -  a=diannaS
    Differential Revision: https://phabricator.services.mozilla.com/D317390

--- security/ct/CTKnownLogs.h
+++ security/ct/CTKnownLogs.h
@@ -14,7 +14,7 @@
 
 #include <stddef.h>
 
-static const PRTime kCTExpirationTime = INT64_C(1791781478000000);
+static const PRTime kCTExpirationTime = INT64_C(1792386188000000);
 
 namespace mozilla::ct {
 
